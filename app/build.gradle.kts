b9c9d774116f05754c962eb55e1e88e45ae9ef18
mdstorrs <141371799+mdstorrs@users.noreply.github.com>
2026-06-16 15:00:48 +1000

    Updated Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = generatedVersionCode
-        versionName = "0.0.3"
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
